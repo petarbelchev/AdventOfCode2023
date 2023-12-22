18d373b206f37d5e0b66a6d1204108349b73b3f0
Petar Belchev <petar.belchev@outlook.com>
2023-12-22 19:07:02 +0200

    Day 5 puzzles

---
+++ Day05/PartOne.cs
@@ -0,0 +1,113 @@
+﻿namespace Day05
+{
+    public static class PartOne
+    {
+        private static long[] _seeds;
+
+        private static readonly string seedToSoil = "seed-to-soil map:";
+        private static readonly string soilToFertilizer = "soil-to-fertilizer map:";
+        private static readonly string fertilizerToWater = "fertilizer-to-water map:";
+        private static readonly string waterToLight = "water-to-light map:";
+        private static readonly string lightToTemperature = "light-to-temperature map:";
+        private static readonly string temperatureToHumidity = "temperature-to-humidity map:";
+        private static readonly string humidityToLocation = "humidity-to-location map:";
+        
+        private static readonly Dictionary<string, List<string>> _mapCollections = new()
+        {
+            { seedToSoil, new List<string>() },
+            { soilToFertilizer, new List<string>() },
+            { fertilizerToWater, new List<string>() },
+            { waterToLight, new List<string>() },
+            { lightToTemperature, new List<string>() },
+            { temperatureToHumidity, new List<string>() },
+            { humidityToLocation, new List<string>() }
+        };
+
+        public static long GetResult(string[] input)
+        {
+            LoadSeeds(input[0]);
+            LoadMaps(input);
+
+            long lowestLocation = long.MaxValue;
+            foreach (var seed in _seeds)
+            {
+                long location = FindLocation(seed);
+
+                if (location < lowestLocation)
+                    lowestLocation = location;
+            }
+
+            return lowestLocation;
+        }
+
+        private static void LoadSeeds(string input)
+            => _seeds = input.Split(": ")[1].Split().Select(long.Parse).ToArray();
+
+        private static void LoadMaps(string[] input)
+        {
+            List<string> currCollection = default;
+
+            for (int line = 2; line < input.Length; line++)
+            {
+                string lineValue = input[line];
+
+                if (string.IsNullOrWhiteSpace(lineValue))
+                    continue;
+
+                if (_mapCollections.TryGetValue(lineValue, out List<string> value))
+                {
+                    currCollection = value;
+                    continue;
+                }
+
+                currCollection.Add(lineValue);
+            }
+        }
+
+        private static long FindLocation(long seed)
+        {
+            long soil = FindMap(seed, _mapCollections[seedToSoil]);
+            if (soil == -1)
+                soil = seed;
+
+            long fertilizer = FindMap(soil, _mapCollections[soilToFertilizer]);
+            if (fertilizer == -1)
+                fertilizer = soil;
+
+            long water = FindMap(fertilizer, _mapCollections[fertilizerToWater]);
+            if (water == -1)
+                water = fertilizer;
+
+            long light = FindMap(water, _mapCollections[waterToLight]);
+            if (light == -1)
+                light = water;
+
+            long temperature = FindMap(light, _mapCollections[lightToTemperature]);
+            if (temperature == -1)
+                temperature = light;
+
+            long humidity = FindMap(temperature, _mapCollections[temperatureToHumidity]);
+            if (humidity == -1)
+                humidity = temperature;
+
+            long location = FindMap(humidity, _mapCollections[humidityToLocation]);
+            if (location == -1)
+                location = humidity;
+
+            return location;
+        }
+
+        private static long FindMap(long source, List<string> map)
+        {
+            for (int i = 0; i < map.Count; i++)
+            {
+                long[] values = map[i].Split().Select(long.Parse).ToArray();
+
+                if (values[1] <= source && source <= values[1] + values[2])
+                    return values[0] + source - values[1];
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Day05/PartTwo.cs
@@ -0,0 +1,118 @@
+﻿namespace Day05
+{
+    static class PartTwo
+    {
+        record Range(long Start, long End);
+
+        class Map
+        {
+            public Range Destination { get; set; }
+            public Range Source { get; set; }
+        }
+
+        private static readonly string seedToSoil = "seed-to-soil map:";
+        private static readonly string soilToFertilizer = "soil-to-fertilizer map:";
+        private static readonly string fertilizerToWater = "fertilizer-to-water map:";
+        private static readonly string waterToLight = "water-to-light map:";
+        private static readonly string lightToTemperature = "light-to-temperature map:";
+        private static readonly string temperatureToHumidity = "temperature-to-humidity map:";
+        private static readonly string humidityToLocation = "humidity-to-location map:";
+
+        private static Dictionary<string, List<Map>> _mapsCollections;
+
+        public static long GetResult(string[] input)
+        {
+            List<Range> startRanges = GetStartRanges(input[0]);
+            LoadMaps(input);
+
+            foreach (var maps in _mapsCollections)
+            {
+                var nextMapRanges = new List<Range>(startRanges);
+                startRanges.Clear();
+
+                var queue = new Queue<Range>(nextMapRanges);
+                nextMapRanges.Clear();
+
+                while (queue.Any())
+                {
+                    Range range = queue.Dequeue();
+                    var map = maps.Value.FirstOrDefault(map => IsMatch(range, map.Source));
+                    if (map is null)
+                    {
+                        nextMapRanges.Add(range);
+                    }
+                    else if (map.Source.Start <= range.Start && range.End <= map.Source.End)
+                    {
+                        long shift = map.Destination.Start - map.Source.Start;
+                        startRanges.Add(new Range(range.Start + shift, range.End + shift));
+                    }
+                    else if (range.Start < map.Source.Start)
+                    {
+                        queue.Enqueue(new Range(range.Start, map.Source.Start - 1));
+                        queue.Enqueue(new Range(map.Source.Start, range.End));
+                    }
+                    else
+                    {
+                        queue.Enqueue(new Range(range.Start, map.Source.End));
+                        queue.Enqueue(new Range(map.Source.End + 1, range.End));
+                    }
+                }
+
+                startRanges.AddRange(nextMapRanges);
+            }
+
+            return startRanges.Min(r => r.Start);
+        }
+
+        static List<Range> GetStartRanges(string input)
+        {
+            long[] values = input.Split(": ")[1].Split().Select(long.Parse).ToArray();
+            List<Range> output = new();
+
+            foreach (long[] seed in values.Chunk(2))
+                output.Add(new Range(seed[0], seed[0] + seed[1] - 1));
+
+            return output;
+        }
+
+        static void LoadMaps(string[] input)
+        {
+            _mapsCollections = new()
+            {
+                { seedToSoil, new List<Map>() },
+                { soilToFertilizer, new List<Map>() },
+                { fertilizerToWater, new List<Map>() },
+                { waterToLight, new List<Map>() },
+                { lightToTemperature, new List<Map>() },
+                { temperatureToHumidity, new List<Map>() },
+                { humidityToLocation, new List<Map>() }
+            };
+
+            List<Map> currCollection = default;
+
+            for (int line = 2; line < input.Length; line++)
+            {
+                string lineValue = input[line];
+
+                if (string.IsNullOrWhiteSpace(lineValue))
+                    continue;
+
+                if (_mapsCollections.TryGetValue(lineValue, out List<Map> value))
+                {
+                    currCollection = value;
+                    continue;
+                }
+
+                long[] lineData = lineValue.Split().Select(long.Parse).ToArray();
+                currCollection.Add(new Map
+                {
+                    Destination = new Range(lineData[0], lineData[0] + lineData[2] - 1),
+                    Source = new Range(lineData[1], lineData[1] + lineData[2] - 1)
+                });
+            }
+        }
+
+        static bool IsMatch(Range range, Range mapSource)
+            => mapSource.Start <= range.End && range.Start <= mapSource.End;
+    }
+}
